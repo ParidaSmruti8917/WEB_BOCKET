eac7766b176b2946ed4069526cbfff9469f915d7
ParidaSmruti8917 <paridasmrutisamapika17@gmail.com>
2024-05-21 18:35:21 +0530

    new js file added

---
+++ nodeJS/class2/Answer.js
@@ -0,0 +1,78 @@
+ //q-1-ans 
+ let Birthday=  new Date ('2000/11/29');
+ console.log(Birthday);
+
+ //q-2-ans
+ let curTime= new Date
+ console.log(curTime.toTimeString());
+
+ //q-3-ans
+
+let Birthday1= new Date('2000/11/29');
+let currentTime = new Date
+console.log(Birthday.currentTime);
+
+//q-4-ans
+//push method used in js for adding new element to the end of an array,it modify the original array also increase its length.
+
+//q-5-ans
+//pop method is used in js to remove an element from the end of an arry.it follow LiFO order.
+
+//q-6-ans
+let fruits=["apple","banana","mango"];
+console.log(fruits);
+fruits.push("orange");
+console.log(fruits);
+
+//q-7-ans
+let num=[1,2,3,4,5];
+num.pop();
+console.log(num);
+
+//q-8-ans
+let color=["red","green","blue"];
+color.push("yellow");
+console.log(color);
+color.pop();
+console.log(color);
+
+//q-9-ans
+let num1=[1,2,3,4]
+num1.push("namita","dhara","bishnu","smruti")
+console.log(num1);
+
+//q-10-ans
+let stack=[];
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
+stack.pop();
+console.log(stack);
+
+//q-11-ans
+let letter=['a','b'];
+letter.push("c","d");
+let lastLetter=letter.pop();
+console.log(letter);
+console.log(lastLetter);
+
+
+//q-12-ans
+let no1=[1,2,3,4,5,6];
+console.log(no1);
+console.log(no1.length);
+no1.push(7,8);
+console.log(no1.length);
+no1.pop();
+console.log(no1.length);
+
+//q-13-ans
+let myAry=[10,20,30];
+myAry.push(40);
+console.log(myAry.pop());
+console.log(myAry);
+
+
+
+
